346cccbc824160c0efdf36813e581715830da476
Derek Oshita <65000424+zetsubou-kira@users.noreply.github.com>
2021-02-18 20:56:19 -0800

    Sub Model, Schema, and getter/setter methods added to models.py.

---
+++ models.py
@@ -0,0 +1,51 @@
+from app import db, marshmallow
+
+# import datetime as dt
+
+# THE BASE CLASS FOR ALL OF YOUR MODELS ARE CALLED db.Model- ITS STORED ON THE SQLALCHEMY INSTANCE YOU HAVE TO CREATE
+class Sub(db.Model): 
+    # TABLE CONFIG: THIS ATTRIBUTE ACCOMMODATES EXISTING TABLES AND APPLIES FUTHER ARGUMENTS WITHIN THE CONSTRUCTOR TO EXISTING TABLE
+    __table_args__ = {'extend_existing': True}
+
+    # WHEN INSTANTIATING A SUB, WE CALL ON THE DB CONSTRUCTOR TO DEFINE THE COLUMNS, ITS NAME, AND WHAT INFORMATION SHOULD BE INSERTED
+    # ONE COLUMN MUST BE DENOTED AS THE PRIMARY-KEY TO CREATE RELATIONSHIPS (unique)
+    id = db.Column(db.Integer, primary_key=True)
+    # 100 REPRESENTS THE MAX LENGTH OF THE STRING 
+    name = db.Column(db.String(100))
+    description = db.Column(db.String(300))
+
+    def __init__(self, name, description): 
+        self.name = name
+        self.description = description 
+
+    # THIS DECORATOR EXISTS SO YOU CAN CREATE CLASS METHODS THAT ARE PASSED THE ACTUAL CLASS OBJ WITHIN THE FUNCTION CALL 
+    @classmethod
+    def create_sub(cls, name, description): 
+        sub = Sub.query.get(subid)
+        new_sub = Sub(name, description)
+        try: 
+            db.session.add(new_sub)
+            db.session.commit()
+        except: 
+            db.session.rollback()
+            raise Exception('Session rollback')
+        return sub_schema.jsonify(new_sub)
+
+    @classmethod 
+    def get_sub(cls, subid): 
+        sub = Sub.query.get(subid)
+        return sub_schema.jsonify(sub)
+
+
+# SCHEMA SET UP USING MARSHMALLOW BECAUSE IT USES CLASSES RATHER THAN DICTIONARIES (EASY CODE REUSE AND CONFIGURATION)
+# YOU CAN USE THIS LIBRARY TO DICTATE WHAT FIELDS WILL BE SENT BACK TO THE USER IN RESPONSE
+class SubSchema(marshmallow.Schema): 
+    class Meta: 
+        fields = ('id', 'name', 'description')
+
+sub_schema = SubSchema()
+subs_schema = SubSchema(many=True)
+
+
+if __name__ == 'models': 
+    db.create_all()
